9f60250db05f3bc42f5b99d10dd6f1397dcdac0e
Patrick Jones <patrickjones.pmj@gmail.com>
2020-03-24 12:39:16 -0500

    modify models to support ActiveRecord

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,15 +1,2 @@
-class Driver
-    attr_accessor :id, :name, :location
-    @@all = []
-
-    def initialize(id, name, location)
-        @id = id
-        @name = name
-        @location = location
-        save
-    end
-
-    def save
-        @@all << self
-    end
+class Driver < ActiveRecord::Base
 end
