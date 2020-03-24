93303cf4b63eeb40d801001b896cda1a73692458
jack_mitchell <jack.danielle.mitchell@gmail.com>
2020-03-24 11:10:59 -0500

    passenger class model

---
+++ .history/lib/passenger_20200323155932.rb
@@ -0,0 +1,8 @@
+class Passenger
+    attr_accessor :location, :destination
+
+    def initialize(location, destination)
+        @location = location
+        @destination = destination
+    end
+end
