5db585bcefdb205fd63c2eb09977a88bcb8ba580
jack_mitchell <jack.danielle.mitchell@gmail.com>
2020-03-24 11:22:38 -0500

    driver and ride classes modeled

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,2 +1,15 @@
 class Driver
+    attr_accessor :id, :name, :location
+    @@all = []
+
+    def initialize(id, name, location)
+        @id = id
+        @name = name
+        @location = location
+        save
+    end
+
+    def save
+        @@all << self
+    end
 end
