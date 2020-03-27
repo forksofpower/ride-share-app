750373be9ed9f33b6c9104a2e04ef653d27c1870
Patrick Jones <patrickjones.pmj@gmail.com>
2020-03-27 09:17:09 -0500

    add welcome message to run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,28 @@
 require_relative '../config/environment'
 
+# begin cli
+
+def start
+    clear_screen
+    welcome_message
+end
 
 
+def welcome_message
+    puts "Hi there!"
+    sleep(2)
+    clear_screen
+end
 
 def new_user_input
     puts "Are you a new passenger?(y/n)"
 end
-binding.pry
+
+def clear_screen
+    system("clear") || system("cls")
+end
+
+start
+
+
+# binding.pry
